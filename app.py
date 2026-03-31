610f004f9ec41f7cc21e6de6994f11598ec15f43
Prateek-Shetty <prateekshetty2552@gmail.com>
2026-03-31 22:32:19 +0530

    URL shorten logic

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+from models import db, URL
+import string, random
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+# Generate short code
+def generate_short_code(length=6):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "URL Shortener Running 🚀"
+    short_url = None
+    if request.method == 'POST':
+        original_url = request.form['url']
+        short_code = generate_short_code()
+
+        new_url = URL(original_url=original_url, short_code=short_code)
+        db.session.add(new_url)
+        db.session.commit()
+
+        short_url = request.host_url + short_code
+
+    return render_template('index.html', short_url=short_url)
+
+@app.route('/<short_code>')
+def redirect_url(short_code):
+    url = URL.query.filter_by(short_code=short_code).first()
+
+    if url:
+        url.clicks += 1
+        db.session.commit()
+        return redirect(url.original_url)
+
+    return "URL not found"
 
 if __name__ == '__main__':
     app.run(debug=True)
